e703c316c33681a9cdf6a583deaada543fcc5a50
John-Kimani <kimwaweru100@gmail.com>
2022-11-04 17:30:58 +0300

    Dev: Apps Create independent application Genereic likes and tags applications

---
+++ tags/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+class Tag(models.Model):
+    '''
+     Class that enable filtering related Food Product instances
+    '''
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    '''
+        Class that enable tag relationship between the foodProduct object
+    '''
+    # Generic relation to other application
+    ## Whats needed objectType and ID
+    ### Query actual object tag is to be applied to
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+

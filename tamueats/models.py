1c18373a5274ab77f6b8f6edffcd470fbf5a2820
John-Kimani <kimwaweru100@gmail.com>
2022-11-04 17:34:40 +0300

    Dev: App - model Define models and database relations

--- tamueats/models.py
+++ tamueats/models.py
@@ -1,3 +1,104 @@
 from django.db import models
 
-# Create your models here.
+class Discounts(models.Model):
+    '''
+     Class that defines promotions and discount on items 
+    '''
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class FoodProductCategory(models.Model):
+    '''
+        Class that defines with category a food product belongs to
+    '''
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('FoodProduct', on_delete=models.SET_NULL, null=True, related_name='+')
+
+
+class FoodProduct(models.Model):
+    '''
+        Class that defines Food Product instance
+    '''
+    food_name = models.CharField(max_length=255)
+    food_description = models.TextField()
+    food_unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    food_category = models.ForeignKey(FoodProductCategory, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Discounts)
+
+
+class Customer(models.Model):
+    '''
+    Class that defines Client instance 
+    '''
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    email_address = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, null=True, blank=True)
+    phone_number = models.CharField(max_length=255)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+
+class FoodOrder(models.Model):
+    '''
+    Class that defince Order instance
+        
+        Args: 
+            Time of order, Payment, customer and optional delivery on pay submission
+    '''
+    DELIVERY_STATUS_PENDING = 'P'
+    DELIVERY_STATUS_DELIVERED = 'D'
+    DELIVERY_STATUS_CHOICES = [
+        (DELIVERY_STATUS_PENDING, 'pending'),
+        (DELIVERY_STATUS_DELIVERED, 'delivered')
+    ]
+
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [ 
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length = 1,
+        choices=PAYMENT_STATUS_CHOICES,
+        default=PAYMENT_STATUS_PENDING,
+    )
+    delivery_status = models.CharField(
+        max_length = 1,
+        choices = DELIVERY_STATUS_CHOICES,
+        default = DELIVERY_STATUS_PENDING,
+        null=True
+    )
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    product = models.ForeignKey(FoodProduct, on_delete=models.PROTECT)
+
+class FoodOrderItem(models.Model):
+    '''
+        Class that handles order processing'
+    '''
+    food_order = models.ForeignKey(FoodOrder, on_delete = models.PROTECT)
+    food_product = models.ForeignKey(FoodProduct, on_delete = models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    '''
+     Class that handles cart operations
+    '''
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    '''
+     Class that handles items in our clients cart
+    '''
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(FoodProduct, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()

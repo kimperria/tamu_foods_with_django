3dcefea76192a29584dbcc4a7422168ce95f79a9
John-Kimani <kimwaweru100@gmail.com>
2022-11-08 15:58:32 +0300

    Dev: App display instances on Admin section

--- tamueats/admin.py
+++ tamueats/admin.py
@@ -6,6 +6,11 @@ class FoodProductAdmin(admin.ModelAdmin):
     '''
         Admin class for foodProduct class
     '''
+    list_display = ['food_name', 'food_unit_price']
+    list_editable = ['food_unit_price']
+    list_per_page = 10
+
+    
 
 
 @admin.register(models.FoodProductCategory)
@@ -16,8 +21,12 @@ class FoodProductCategoryAdmin(admin.ModelAdmin):
 
 
 
+
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     '''
     Admin class for customer 
     '''
+    list_display = ['first_name', 'last_name', 'username','email_address', 'phone_number', 'registration_date']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name']

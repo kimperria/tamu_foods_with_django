0b28e788dd6a69e73484794af6021952b910e88c
John-Kimani <kimwaweru100@gmail.com>
2022-11-08 15:38:00 +0300

    Dev: Register models in admin section

--- tamueats/admin.py
+++ tamueats/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.FoodProduct)
+class FoodProductAdmin(admin.ModelAdmin):
+    '''
+        Admin class for foodProduct class
+    '''
+
+
+@admin.register(models.FoodProductCategory)
+class FoodProductCategoryAdmin(admin.ModelAdmin):
+    '''
+        Admin class for category
+    '''
+
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    '''
+    Admin class for customer 
+    '''

aaa342896f0baaa72eb04decd86974f611484636
John-Kimani <kimwaweru100@gmail.com>
2022-11-12 02:40:26 +0300

    Dev: creating user profile

--- tamueats/admin.py
+++ tamueats/admin.py
@@ -99,9 +99,11 @@ class CustomerAdmin(admin.ModelAdmin):
     Admin class for customer 
         search fields allow case sensitive search of character
     '''
-    list_display = ['first_name', 'last_name', 'username','email_address', 'phone_number', 'registration_date']
+    list_display = ['first_name', 'last_name', 'username', 'registration_date']
     list_per_page = 10
-    ordering = ['first_name', 'last_name']
+    #eagerloading user data
+    list_select_related = ['user']
+    ordering = ['user__first_name', 'user__last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
 
--- tamueats/models.py
+++ tamueats/models.py
@@ -1,3 +1,5 @@
+from django.contrib import admin
+from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 
@@ -50,18 +52,26 @@ class Customer(models.Model):
     '''
     Class that defines Client instance 
     '''
-    first_name = models.CharField(max_length=255, blank=True, null=True)
-    last_name = models.CharField(max_length=255, blank=True, null=True)
-    email_address = models.EmailField(unique=True)
     username = models.CharField(max_length=150, null=True, blank=True)
     phone_number = models.CharField(max_length=255)
     registration_date = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f' {self.first_name} {self.last_name}'
+        return f' {self.user.first_name} {self.user.last_name}'
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
 
     class Meta:
-        ordering = ['first_name', 'last_name']
+        # Double underscore to reference user instance
+        ordering = ['user__first_name', 'user__last_name']
 
 class FoodOrder(models.Model):
     '''

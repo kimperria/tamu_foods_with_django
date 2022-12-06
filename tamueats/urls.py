c6f3f0fb92110289a31fa0648ee6bbca31e84a24
John-Kimani <kimwaweru100@gmail.com>
2022-12-07 00:07:42 +0300

    Prod: revamp home url

--- tamueats/urls.py
+++ tamueats/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.index, name='HomePage'),
+    path('', views.index, name='HomePage'),
     path('menu/', views.menu_page, name='MenuPage'),
     path('cart/', views.cart_page, name='cart'),
     path('checkout/', views.checkout_page, name='checkout'),
